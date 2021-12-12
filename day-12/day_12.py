b7c03b695b33fbd65f2d2bc324dedbc2af221bb2
jbburt <joshuaburtphd@gmail.com>
2021-12-12 16:42:36 -0500

    gotta go FAST v3

--- day-12/day_12.py
+++ day-12/day_12.py
@@ -5,34 +5,38 @@ from collections import defaultdict
 from collections import deque
 
 edges = defaultdict(set)
+vertices = set()
 with open('day-12/input.txt', 'r') as fp:
     for line in fp.read().split('\n'):
         a, b = line.split('-')
+        vertices.add(a)
+        vertices.add(b)
         if a != 'end' and b != 'start':
             edges[a].add(b)
         if a != 'start' and b != 'end':
             edges[b].add(a)
+isupper = {k: k.isupper() for k in vertices}
 
 npaths = 0
-stack = deque([('start',)])
+stack = deque([({'start'}, 'start')])
 while stack:
-    path = stack.pop()
-    for there in edges[path[-1]]:
+    path, last = stack.pop()
+    for there in edges[last]:
         if there == 'end':
             npaths += 1
-        elif there not in path or there.isupper():
-            stack.append(path + (there,))
+        elif isupper[there] or there not in path:
+            stack.append((path.union({there}), there))
 print(f'problem 1: {npaths}')
 
 npaths = 0
-stack = deque([(False, 'start')])
+stack = deque([(False, {'start'}, 'start')])
 while stack:
-    path = stack.pop()
-    for there in edges[path[-1]]:
+    has_double, path, last = stack.pop()
+    for there in edges[last]:
         if there == 'end':
             npaths += 1
-        elif there.isupper() or there not in path:
-            stack.append(path + (there,))
-        elif not path[0]:
-            stack.append((True,) + path[1:] + (there,))
+        elif isupper[there] or there not in path:
+            stack.append((has_double, path.union({there}), there))
+        elif not has_double:
+            stack.append((True, path.union({there}), there))
 print(f'problem 2: {npaths}')
