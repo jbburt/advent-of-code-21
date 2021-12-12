62d160d23b0e02250a9fb779c0df23b5bc152a59
jbburt <joshuaburtphd@gmail.com>
2021-12-12 16:26:05 -0500

    gotta go FAST v2

--- day-12/day_12.py
+++ day-12/day_12.py
@@ -14,25 +14,25 @@ with open('day-12/input.txt', 'r') as fp:
             edges[b].add(a)
 
 npaths = 0
-stack = {('start',)}
+stack = deque([('start',)])
 while stack:
     path = stack.pop()
     for there in edges[path[-1]]:
         if there == 'end':
             npaths += 1
         elif there not in path or there.isupper():
-            stack.add(path + (there,))
+            stack.append(path + (there,))
 print(f'problem 1: {npaths}')
 
 npaths = 0
-stack = {(False, 'start')}
+stack = deque([(False, 'start')])
 while stack:
     path = stack.pop()
     for there in edges[path[-1]]:
         if there == 'end':
             npaths += 1
         elif there.isupper() or there not in path:
-            stack.add(path + (there,))
+            stack.append(path + (there,))
         elif not path[0]:
-            stack.add((True,) + path[1:] + (there,))
+            stack.append((True,) + path[1:] + (there,))
 print(f'problem 2: {npaths}')
