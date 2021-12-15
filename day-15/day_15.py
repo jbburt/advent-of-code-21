5bb5cd2e47e280a64e1ca020a3b539d5b2327953
jbburt <joshuaburtphd@gmail.com>
2021-12-15 18:27:42 -0500

    still terrible but 30x faster

--- day-15/day_15.py
+++ day-15/day_15.py
@@ -1,7 +1,13 @@
 """
 https://adventofcode.com/2021/day/15
 """
+import heapq as hq
+import itertools
+from time import time
+
 import numpy as np
+
+t0 = time()
 
 with open('day-15/input.txt', 'r') as fp:
     local = np.array([list(map(int, line)) for line in fp.read().split('\n')])
@@ -11,31 +17,30 @@ local[0, 0] = 0
 
 deltas = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-
-def main(grid):
-    nrows, ncols = grid.shape
-    unvisited = np.ones(grid.shape, dtype=bool)
-    dist = np.array([np.inf] * nrows * ncols).reshape(grid.shape)
-    dist[0, 0] = 0
-    while unvisited.any():
-        min_dist = dist[unvisited].min()
-        v = tuple(x[0] for x in np.where(np.logical_and(unvisited, dist == min_dist)))
-        unvisited[v] = False
-        i, j = v
-        for di, dj in deltas:
-            u = (i + di, j + dj)
-            if 0 <= u[0] <= nrows - 1 and 0 <= u[1] <= ncols - 1:
-                if unvisited[u]:
-                    dist_to_u = dist[v] + grid[u]
-                    if dist_to_u < dist[u]:
-                        dist[u] = dist_to_u
-    return dist
-
-
-d = main(local)
-print(f'p1: {d[-1, -1]}')
-
 nr, nc = local.shape
+
+pairs = set(list(itertools.product(range(nr), range(nc))))
+dist = {point: float('inf') for point in pairs}
+dist[(0, 0)] = 0
+d = [(0, (0, 0))] + [(float('inf'), pair) for pair in pairs]
+
+d = list(sorted(d, key=lambda z: z[0]))
+hq.heapify(d)
+unvisited = pairs.copy()
+while unvisited:
+    dist_to_v, v = hq.heappop(d)
+    unvisited.remove(v)
+    i, j = v
+    for di, dj in deltas:
+        u = (i + di, j + dj)
+        if u in unvisited and 0 <= u[0] <= nr - 1 and 0 <= u[1] <= nc - 1:
+            dist_to_u = dist[v] + local[u]
+            if dist_to_u < dist[u]:
+                dist[u] = dist_to_u
+                hq.heappush(d, (dist[u], u))
+
+print('p1:', dist[(nr - 1, nc - 1)])
+
 local2 = local.copy()
 local2[0, 0] = start
 local2 = np.tile(local2, (5, 5))
@@ -44,9 +49,30 @@ x = (np.arange(nr * 5) / nr).astype(int)
 increment = np.add.outer(x, x).astype(int)
 
 local2 += increment.astype(int)
-local2 = np.mod(local2-1, 9)+1
-local2[0, 0] = 0
+local2 = np.mod(local2 - 1, 9) + 1
 
-d2 = main(local2)
-print(f'p2: {d2[-1, -1]}')
+nr, nc = local2.shape
 
+pairs = set(list(itertools.product(range(nr), range(nc))))
+dist = {point: float('inf') for point in pairs}
+dist[(0, 0)] = 0
+d = [(0, (0, 0))] + [(float('inf'), pair) for pair in pairs]
+
+d = list(sorted(d, key=lambda z: z[0]))
+hq.heapify(d)
+unvisited = pairs.copy()
+
+while unvisited:
+    dist_to_v, v = hq.heappop(d)
+    unvisited.remove(v)
+    i, j = v
+    for di, dj in deltas:
+        u = (i + di, j + dj)
+        if u in unvisited and 0 <= u[0] <= nr - 1 and 0 <= u[1] <= nc - 1:
+            dist_to_u = dist[v] + local2[u]
+            if dist_to_u < dist[u]:
+                dist[u] = dist_to_u
+                hq.heappush(d, (dist[u], u))
+
+print('p2:', dist[(nr - 1, nc - 1)])
+print('time:', time() - t0)
