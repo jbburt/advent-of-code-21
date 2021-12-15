5ca6ba3671f162166666f157ce95de4b046d67e6
jbburt <joshuaburtphd@gmail.com>
2021-12-14 20:10:40 -0500

    faster

--- day-14/day_14.py
+++ day-14/day_14.py
@@ -7,49 +7,45 @@ from itertools import product
 with open('day-14/input.txt', 'r') as fp:
     lines = fp.read().split('\n')
     template = lines[0]
-    rules = dict()
-    for line in lines[2:]:
-        k, v = line.split(' -> ')
-        rules[k] = v
+    rules = {k: v for (k, v) in map(lambda x: x.split(' -> '), lines[2:])}
 
-# p1
-tmp = template
-for i in range(10):
-    tmp = "".join(t + rules.get(t + tmp[i + 1], '') for i, t in enumerate(tmp[:-1])) + \
-          tmp[-1]
-counts = {c: tmp.count(c) for c in set(tmp)}
-_, n = zip(*counts.items())
-print(f'p1: {max(n) - min(n)}')
-
-# p2
+# all unique characters
 chars = set(template)
 for k, v in rules.items():
-    chars |= set(k)
-    chars |= set(v)
+    chars |= set(k).union(v)
 
+# running tally of the number of each character pair
 pairs = set(map(lambda x: "".join(a for a in x), list(product(chars, chars))))
 npairs = {p: 0 for p in pairs}
 for i in range(len(template) - 1):
     npairs[template[i:i + 2]] += 1
 
-for i in range(40):
-    new_npairs = npairs.copy()
-    for pair, n in npairs.items():
-        if pair in rules:
-            new_npairs[pair[0] + rules[pair]] += n
-            new_npairs[rules[pair] + pair[1]] += n
-            new_npairs[pair] -= n
-    npairs = new_npairs
 
-counts = {c: 0 for c in chars}
-for k, v in npairs.items():
-    for c in k:
-        counts[c] += v
+def solve(pair_count: dict) -> int:
+    char_count = {c: 0 for c in chars}
+    for k, v in pair_count.items():
+        for c in k:
+            char_count[c] += v
+    for c in chars:
+        start = sum(pair_count[p] for p in pairs if p[0] == c)
+        end = sum(pair_count[p] for p in pairs if p[1] == c)
+        char_count[c] -= min(start, end)
+    _, n = zip(*char_count.items())
+    return max(n) - min(n)
 
-for c in chars:
-    start = sum(npairs[p] for p in pairs if p[0] == c)
-    end = sum(npairs[p] for p in pairs if p[1] == c)
-    counts[c] -= min(start, end)
-_, n = zip(*counts.items())
-print(f'p2: {max(n) - min(n)}')
 
+def simulate(n: int, pair_count: dict) -> dict:
+    for _ in range(n):
+        new_np = pair_count.copy()
+        for pair, ct in pair_count.items():
+            if pair in rules:
+                new_np[pair[0] + rules[pair]] += ct
+                new_np[rules[pair] + pair[1]] += ct
+                new_np[pair] -= ct
+        pair_count = new_np
+    return pair_count
+
+
+temp = simulate(10, npairs.copy())
+print(f'p1: {solve(temp)}')
+print(f'p2: {solve(simulate(30, temp))}')
